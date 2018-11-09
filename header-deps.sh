f95c14a919d953a27acb7d3abaed04e1e623943e
Jason Pepas <jasonpepas@gmail.com>
2018-11-09 04:19:30 -0600

    simplifying

--- header-deps.sh
+++ header-deps.sh
@@ -11,47 +11,37 @@ repo_url=https://raw.githubusercontent.com/pepaslabs/header-deps.sh/master/heade
 
 set -e -o pipefail
 
-test -e header-deps.txt \
-    || (echo "Error: header-deps.txt not found." >&2 ; exit 1)
-
-deps_line_count=`wc -l header-deps.txt | awk '{ print $1 }'` 
+if [ $# -eq 0 ]
+then
+    cat header-deps.txt | xargs header-deps.sh
+    exit 0
+fi
 
 repo_file=`mktemp`
 curl -s $repo_url >> $repo_file
 
-deps_line_num=1
-while test $deps_line_num -le $deps_line_count
+for dep in $@
 do
-    dep_header=`head -n$deps_line_num header-deps.txt | tail -n1`
     found_dep="no"
-
-    repo_line_num=1
-    repo_line_count=`wc -l $repo_file | awk '{ print $1 }'`
-    while test $repo_line_num -le $repo_line_count
+    while read -r repo_line
     do
-        repo_line=`head -n$repo_line_num $repo_file | tail -n1`
         if [ "`echo $repo_line | head -c4`" == "http" ]
         then
             base_url=$repo_line
         else
-            repo_header=$repo_line
-            if [ "$dep_header" == "$repo_header" ]
+            if [ "$dep" == "$repo_line" ]
             then
-                url="${base_url}${dep_header}"
+                url="${base_url}${dep}"
                 echo "GET $url"
                 curl -s -O $url
                 found_dep="yes"
                 break
             fi
         fi
-        
-        repo_line_num=$(( $repo_line_num + 1 ))
-    done
+    done < $repo_file
 
     test "$found_dep" == "yes" \
-        || (echo "Error: can't find $dep_pkg." >&2 ; exit 1)
-
-    deps_line_num=$(( $deps_line_num + 1 ))
+        || (echo "Error: can't find $dep." >&2 ; exit 1)
 done
 
 rm -f $repo_file
